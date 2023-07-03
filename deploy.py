2e67472c68508b2f26b276a46f80d6de33533c00
Aasneh <96459729+Aasneh@users.noreply.github.com>
2023-07-03 20:50:09 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -94,7 +94,7 @@ st.header("TEST IT YOURSELF :smile:")
 
 test_path="./Test/"
 upload = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
-if upload is not None:
+# if upload is not None:
     # image_bytes = upload.read()
     # st.image(image_bytes, caption="Uploaded Image", use_column_width=True)
     # with open(os.path.join(test_path,"test.jpg"),"wb") as f: 
