98dd4b9e36c4e5c1b7e49f0e92b02131219522bc
Aasneh <96459729+Aasneh@users.noreply.github.com>
2023-07-03 20:23:04 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,23 +1,23 @@
-import pandas as pd
+# import pandas as pd
 import numpy as np
 import tensorflow as tf
-from matplotlib import pyplot as plt
-import cv2 as cv
+# from matplotlib import pyplot as plt
+# import cv2 as cv
 import os
-import PIL
+# import PIL
 from PIL import Image, ImageOps
-import pickle
-import pathlib
-import seaborn as sns
+# import pickle
+# import pathlib
+# import seaborn as sns
 from tensorflow import keras
-from tensorflow.keras import datasets, layers, models
+# from tensorflow.keras import datasets, layers, models
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Model
-from tensorflow.keras.utils import to_categorical, plot_model
-from tensorflow.keras.layers import Input, Dense, LSTM, Embedding, Dropout, add
+# from tensorflow.keras.utils import to_categorical, plot_model
+# from tensorflow.keras.layers import Input, Dense, LSTM, Embedding, Dropout, add
 import streamlit as st
 
 img_model=VGG16()
